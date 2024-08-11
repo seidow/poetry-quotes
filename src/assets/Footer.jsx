abc4d45163775864b380e11301304836f9e26bb6
Saed Eidow <seidow@stu.kau.edu.sa>
2024-08-11 14:20:25 +0300

    Modify Footer

--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
     const year = new Date().getFullYear();
     return (
       <footer>
-        <p className="focus:outline-none focus:shadow-outline">Copyright ⓒ {year} <strong> Saed </strong> </p>
+        <p className="focus:outline-none focus:shadow-outline">Copyright ⓒ {year} developed by <strong> Saed </strong> </p>
       </footer>
     );
   }
