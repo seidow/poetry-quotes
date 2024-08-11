ecfa05870413e3c5d6d80ef8de913e87e0d06458
Saed Eidow <seidow@stu.kau.edu.sa>
2024-08-11 16:59:45 +0300

    Update header, footer, and add favicon

--- src/assets/Header.jsx
+++ src/assets/Header.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
-
+import logo from '../images/logo.png'
 
 const Header = () => {
     return (
         <header className="py-4 px-6 text-2xl md:text-4l lg:text-6xl w-full text-center font-medium">
-            
+            <img className="logo" src={logo} alt="logo" />
             <h1>اقتباسات من الأدب العربي</h1>
-            <FormatQuoteIcon color="primary" fontSize="large" />
         </header>
     )
 }
