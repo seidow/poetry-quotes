3ddd269658472b04648e571a252270aec8a620e5
Saed Eidow <saeededow@gmail.com>
2024-08-24 21:09:42 +0300

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -43,9 +43,9 @@ function App() {
 
 
   return (
-    <div className="flex flex-col min-h-screen bg-[url('./images/wall-background.jpg')] bg-cover bg-no-repeate">
+    <div className="flex flex-col min-h-screen bg-[url('./images/wall-background.jpg')] bg-cover bg-fixed bg-no-repeat">
       {/* Header */}
-      <header className="flex items-center justify-center text-center py-4 px-6 bg-gray-900">
+      <header className="flex items-center justify-center text-center py-4 px-6 bg-gradient-to-r from-gray-900 to-gray-800 shadow-md">
         <img className="w-16 md:w-24 lg:w-32 invert mr-4" src={logo} alt="logo" />
         <h1 className="font-Kufi text-2xl md:text-4xl lg:text-5xl text-white">
           اقتباسات من الأدب العربي
@@ -54,12 +54,12 @@ function App() {
 
       {/* Main Content */}
       <main className="flex-grow flex items-center justify-center">
-        <div className="relative flex flex-col items-center justify-center  w-11/12 md:w-2/3 lg:w-1/2 bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900 text-white rounded-lg p-8 shadow-lg">
+        <div className="relative flex flex-col items-center justify-center w-11/12 md:w-2/3 lg:w-1/2 bg-gradient-to-r from-gray-700 via-gray-800 to-gray-900 text-white rounded-lg p-8 shadow-lg">
           <div className="text-2xl md:text-4xl lg:text-5xl mb-4 text-center leading-relaxed">
-            <h1 className='leading-loose quote-text'>{currentPoetry.text1}</h1>
-            <h1 className='quote-text transition-opacity duration-300'>{currentPoetry.text2}</h1>
+            <h1 className="leading-loose quote-text">{currentPoetry.text1}</h1>
+            <h1 className="quote-text transition-opacity duration-300">{currentPoetry.text2}</h1>
           </div>
-          <h2 className="text-xl md:text-2xl lg:text-3xl mt-4">{currentPoetry.author}</h2>
+          <h2 className="text-xl md:text-2xl lg:text-3xl mt-4 text-orange-500 font-semibold">{currentPoetry.author}</h2>
 
           {/* Refresh Icon */}
           <button
@@ -89,7 +89,7 @@ function App() {
       </main>
 
       {/* Footer */}
-      <footer className="absolute bottom-0 right-0 left-0 bg-gray-800 text-white py-4">
+      <footer className="absolute bottom-0 right-0 left-0 bg-gray-800 text-white py-4 shadow-inner">
         <div className="text-center">
           <p>
             Copyright &copy; {new Date().getFullYear()} Developed by <strong>Saed</strong>
